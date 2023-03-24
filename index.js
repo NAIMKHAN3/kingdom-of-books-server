47c09b73cc5cb4496fd7c7763775a42f4cc53b4c
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-03-24 16:29:38 +0600

    books get successfully

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send({ status: true, Message: 'Moon Tech Server is Running' })
 })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujhfrio.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const booksCollections = client.db("kingdom-books").collection("books")
+        try {
+            app.get('/books', async (req, res) => {
+                const result = await booksCollections.find({}).toArray();
+                res.send(result)
+            })
+        }
+        catch {
+            res.send({ status: false, message: "Products Not Found" })
+        }
+
+    }
+    catch {
+        res.send({ status: false, Message: "Server Is Down" })
+    }
+
+}
+run().catch(e => console.log(e))
+
+
+
 app.listen(port, () => {
     console.log("Moon Tech Server is running")
 })
